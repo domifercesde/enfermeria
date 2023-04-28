3b66e9359e090b2789cd94b882aea502c1cfa232
Your Name <you@example.com>
2023-04-28 11:04:20 -0500

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
+import { BrowserRouter } from 'react-router-dom'
+import { Menu } from './Menu/Menu'
+import { Footer } from './Footer/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Menu></Menu>
+    <Footer />
+    </BrowserRouter>
   </React.StrictMode>,
 )
