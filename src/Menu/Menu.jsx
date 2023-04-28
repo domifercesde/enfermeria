48c1036f3fcefe44b236dbd048d50a70ec71f282
roxy0825 <roxyg261@gmail.com>
2023-04-28 11:42:03 -0500

    corregir colores, titulos, nombres y agregar iconos

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -1,24 +1,22 @@
 import { Link } from "react-router-dom"
+import './Menu.css'
 export function Menu(){
     return(
         <>
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-black navbar-dark">
+            <nav class="navbar navbar-expand-lg navbar-light navbar-dark menuc">
                 <div class="container-fluid ">
                     <Link to="/" className="navbar-brand"><img src="https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/logoDomifer.png?alt=media&token=a9dfb486-4a30-4cde-ac55-65e2fc0a4018" alt="logo" width="45" height="40" className="rounded"/></Link>                   
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+                                <Link to="/" className="nav-link active" aria-current="page">Inicio</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/integrantes" className="nav-link">Miembros</Link>
-                            </li>
+                                <Link to="/integrantes" className="nav-link">Formulario</Link>
+                            </li>                            
                             <li className="nav-item">
-                                <Link to="/tienda" className="nav-link">Tienda</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/musica" className="nav-link" tabindex="-1" aria-disabled="true">Musica</Link>
+                                <Link to="/musica" className="nav-link" tabindex="-1" aria-disabled="true">Servicios</Link>
                             </li>
                         </ul>
                     </div>
